23e564654693160e2f40e7f0dfb293835e5b9541
mkpark-VIRNECT <95333956+mkpark-VIRNECT@users.noreply.github.com>
2022-03-07 14:44:18 +0900

    .

---
+++ SpeechToText_AppleAPI/Assets/PermissionChecker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class PermissionChecker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start ()
+    {
+        StartCoroutine(CheckPermission());
+    }
+    IEnumerator CheckPermission ()
+    {
+        yield return new WaitForEndOfFrame();
+        int permissionCheckCount = 0;
+        var permissionCallbacks = new PermissionCallbacks();
+        permissionCallbacks.PermissionDenied += s =>
+        {
+            Debug.LogError($"Permission Denied : {s}");
+            permissionCheckCount++;
+        };
+        permissionCallbacks.PermissionDeniedAndDontAskAgain += s =>
+        {
+            Debug.LogError($"Permission Denied and dont ask again : {s}");
+            permissionCheckCount++;
+        };
+        permissionCallbacks.PermissionGranted += s =>
+        {
+            Debug.Log($"Permission Granted : {s}");
+            permissionCheckCount++;
+        };
+
+        Permission.RequestUserPermissions(new string[] { Permission.ExternalStorageRead, Permission.ExternalStorageWrite, Permission.Camera }, permissionCallbacks);
+
+        while ( permissionCheckCount < 3 )
+        {
+            yield return null;
+        }
+    }
+}
